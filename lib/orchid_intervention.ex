8b09cc902da87651b631f05c3980af246cad31b4
RoastedChestnut <chestnut.roasted@outlook.com>
2026-03-28 17:24:00 +0800

    type: Add some type declaration

--- lib/orchid_intervention.ex
+++ lib/orchid_intervention.ex
@@ -2,4 +2,9 @@ defmodule OrchidIntervention do
   @moduledoc """
   Documentation for `OrchidIntervention`.
   """
+
+  @type intervention_type :: :input | :override | atom()
+  @type payload :: (-> Orchid.Param.t()) | Orchid.Param.t() | term()
+
+  @type interventions :: %{Orchid.Step.io_key() => %{intervention_type => payload}}
 end
